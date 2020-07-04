a56b226f51f8d475bc120aed09d64e9f341f0e5d
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-07-04 16:49:57 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '29cad0992871c782c0376162e3b5c5a888b484ba86ad7816ce1cecdb8b9a1b31003f49b7a14da7ca36c849282bbc8edbafb04caf209f9c5f7862fd38ce481ceb'
